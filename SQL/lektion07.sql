69c537793e89444ef68b8f10bd036d06b6f7c8aa
Martin Haagen <martin@mbh.se>
2024-01-12 09:40:53 +0100

    Uppdaterad SQL-fil

--- SQL/lektion07.sql
+++ SQL/lektion07.sql
@@ -11,3 +11,24 @@ SELECT * FROM Contacts;
 
 
 SELECT name FROM Contacts UNION SHOW TABLES;
+
+
+SELECT SHA2('X', 256);
+
+
+CREATE TABLE Users (
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+	username VARCHAR(255),
+	password VARCHAR(255),
+	salt VARCHAR(255)
+);
+
+
+SELECT ROUND(RAND()*10000000000);
+
+INSERT INTO Users (username, password, salt) VALUE ('user1', SHA2(CONCAT('Martin','3316955523'), 256), '3316955523');
+
+SELECT * FROM Users;
+
+SELECT id, username FROM Users WHERE username = 'user1' AND password = SHA2(CONCAT('Martin', salt), 256);
+
